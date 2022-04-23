8de2e2b446f888643d29c4b89972e1f8ae8f7cf6
Andy Grunwald <andygrunwald@gmail.com>
2022-04-23 18:53:36 +0200

    Basic integration of tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
 	// Enable the Preact integration to support Preact JSX components.
-	integrations: [preact()],
+	integrations: [
+		preact(),
+		tailwind()
+	],
 });
