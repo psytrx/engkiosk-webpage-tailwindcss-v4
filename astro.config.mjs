2bceaa3297a643443dee01b391c00170faab854c
Andy Grunwald <andygrunwald@gmail.com>
2022-05-28 22:18:51 +0200

    Integrated sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,6 +10,7 @@ export default defineConfig({
 	// Enable the Preact integration to support Preact JSX components.
 	integrations: [
 		preact(),
-		tailwind()
+		tailwind(),
+		sitemap()
 	],
 });
