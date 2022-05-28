7ac4e14aefd613e6047bc7b4ff12b9965924ec1c
Andy Grunwald <andygrunwald@gmail.com>
2022-05-28 20:59:37 +0200

    Set site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://engineeringkiosk.dev',
+
 	// Enable the Preact integration to support Preact JSX components.
 	integrations: [
 		preact(),
