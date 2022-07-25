fb42701df3a95b0e7be77f0b8d03782fcf40522f
Andy Grunwald <andygrunwald@gmail.com>
2022-07-25 21:22:16 +0200

    Extract formatDate into own file

---
+++ src/scripts/date.js
@@ -0,0 +1,19 @@
+export function formatDate(date) {
+	const options = {
+		weekday: 'long',
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric'
+	};
+	return new Date(date).toLocaleDateString('de-DE', options);
+}
+
+export function formatUnixTimestampToDate(timestamp) {
+	const options = {
+		weekday: 'long',
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric'
+	};
+	return new Date(timestamp * 1000).toLocaleDateString('de-DE', options);
+}
