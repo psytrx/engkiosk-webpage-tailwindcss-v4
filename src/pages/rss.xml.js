2107ffa14e0b3f2aadc90844c5ea3e70746f23af
Andy Grunwald <andygrunwald@gmail.com>
2022-08-03 13:47:21 +0200

    First try ...

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,12 @@
 import rss from '@astrojs/rss';
 
+const globItems  = import.meta.glob([
+    './blog/post/*.md', 
+    './podcast/episode/*.md'
+]);
+const items = Object.values(globItems);
+console.log(items)
+
 // Docs: https://docs.astro.build/en/guides/rss/
 export const get = () => rss({
     title: 'Engineering Kiosk',
@@ -7,8 +14,9 @@ export const get = () => rss({
     site: import.meta.env.SITE,
     // Currently hitting a bug here
     // See https://github.com/withastro/astro/issues/3946
-    items: import.meta.glob([
-        './blog/post/*.md', 
-        './podcast/episode/*.md'
-    ])
+    items: items.map((post) => ({
+        //link: post.frontmatter.slug,
+        title: post.frontmatter.title,
+        pubDate: post.frontmatter.date,
+    }))
   });
