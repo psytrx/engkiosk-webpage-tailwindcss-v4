f4da3f0ae4592e2d580c73c34f6ca2aa9f49388f
Andy Grunwald <andygrunwald@gmail.com>
2022-10-14 20:51:03 +0200

    rss feed: Get first version with blog post running

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,22 +1,14 @@
 import rss from '@astrojs/rss';
 
-const globItems  = import.meta.glob([
-    './blog/post/*.md', 
-    './podcast/episode/*.md'
+const rssItems  = import.meta.glob([
+    './blog/post/*.mdx',
 ]);
-const items = Object.values(globItems);
-console.log(items)
 
 // Docs: https://docs.astro.build/en/guides/rss/
 export const get = () => rss({
     title: 'Engineering Kiosk',
     description: 'Der deutschsprachige Software-Engineering-Podcast mit Wolfgang Gassler und Andy Grunwald rund um die Themen Engineering-Kultur, Open Source und Technologie.',
     site: import.meta.env.SITE,
-    // Currently hitting a bug here
-    // See https://github.com/withastro/astro/issues/3946
-    items: items.map((post) => ({
-        //link: post.frontmatter.slug,
-        title: post.frontmatter.title,
-        pubDate: post.frontmatter.date,
-    }))
-  });
+    items: rssItems,
+    customData: `<language>de-de</language>`,
+});
