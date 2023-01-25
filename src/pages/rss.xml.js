ecb3b6e344ae1e191d542e81c25b8ecdc1eec1a9
Andy Grunwald <andygrunwald@gmail.com>
2023-01-25 22:59:52 +0100

    Fix rss handling: Deprecated import.meta.glob

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,15 +1,15 @@
-import rss from '@astrojs/rss';
-
-const rssItems  = import.meta.glob([
-    './blog/post/*.mdx',
-    './podcast/episode/*.md'
-]);
+import rss, {pagesGlobToRssItems} from '@astrojs/rss';
 
 // Docs: https://docs.astro.build/en/guides/rss/
 export const get = () => rss({
     title: 'Engineering Kiosk',
     description: 'Der deutschsprachige Software-Engineering-Podcast mit Wolfgang Gassler und Andy Grunwald rund um die Themen Engineering-Kultur, Open Source und Technologie.',
     site: import.meta.env.SITE,
-    items: rssItems,
+    items: pagesGlobToRssItems(
+        import.meta.glob([
+            './blog/post/*.mdx',
+            './podcast/episode/*.md'
+        ])
+    ),
     customData: `<language>de-de</language>`,
 });
