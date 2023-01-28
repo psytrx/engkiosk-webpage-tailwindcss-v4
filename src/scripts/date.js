a193e1312275cf3b378926ba270649b149c44657
Andy Grunwald <andygrunwald@gmail.com>
2023-01-28 21:22:40 +0100

    Make curent podcast episode prominent on the startpage

--- src/scripts/date.js
+++ src/scripts/date.js
@@ -17,3 +17,12 @@ export function formatUnixTimestampToDate(timestamp) {
 	};
 	return new Date(timestamp * 1000).toLocaleDateString('de-DE', options);
 }
+
+export function formatDateWithoutWeekday(date) {
+	const options = {
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric'
+	};
+	return new Date(date).toLocaleDateString('de-DE', options);
+}
