0d10425637b323ecf14cbf0265513a7c13922e53
Andy Grunwald <andygrunwald@gmail.com>
2023-09-09 15:51:32 +0200

    Update RSS integration: https://docs.astro.build/en/guides/rss/

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,11 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 // Docs: https://docs.astro.build/en/guides/rss/
-export async function get() {
+export async function GET(context) {
 	return rss({
 		title: 'Engineering Kiosk',
 		description: 'Der deutschsprachige Software-Engineering-Podcast mit Wolfgang Gassler und Andy Grunwald rund um die Themen Engineering-Kultur, Open Source und Technologie.',
-		site: import.meta.env.SITE,
+		site: context.site,
 		items: await pagesGlobToRssItems(import.meta.glob(['./blog/post/*.mdx', './podcast/episode/*.md'])),
 		customData: `<language>de-de</language>`,
 	});
