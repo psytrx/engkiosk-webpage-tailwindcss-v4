2691b0b83c689f09c461187942aa1b8912df5ec2
Wolfgang Gassler <wolfgang@gassler.org>
2023-09-26 17:51:36 +0200

    use shortlink piwik campaign for shortlinks /epXY

--- scripts/podcast_feed_to_content.py
+++ scripts/podcast_feed_to_content.py
@@ -470,7 +470,7 @@ def create_redirects(file_to_parse, path_md_files, redirect_prefix):
         #   - https://engineeringkiosk.dev/ep5?pkn=twit_init
         new_redirect_episode_shortlink = {
             "from": f"/ep{episode_number}",
-            "to": f"/podcast/episode/{episode_file}",
+            "to": f"/podcast/episode/{episode_file}?pkn=shortlink",
             "status": 301,
             "force": True,
         }
