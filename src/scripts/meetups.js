d3b4c094596bfb90c5fb147a8f944c10343958fe
Wolfgang Gassler <wolfgang@gassler.org>
2023-12-15 13:15:54 +0100

    wait 2 days until show next meetup

--- src/scripts/meetups.js
+++ src/scripts/meetups.js
@@ -2,18 +2,31 @@ import { getCollection } from 'astro:content';
 
 let allMeetups = await getCollection('meetup');
 allMeetups = allMeetups.map((meetup) => meetup.data);
-const timeDivider = new Date(new Date().setHours(23, 59, 59, 999)).getTime();
+const todayEndOfDay = new Date(new Date().setHours(23, 59, 59, 999)).getTime();
 
-export function getNextMeetups(limit = undefined) {
+export function getNextMeetups(limit = undefined, timeDivider = undefined) {
+	if (!timeDivider) {
+		timeDivider = todayEndOfDay;
+	}
 	const meetups = allMeetups.filter((meetup) => new Date(meetup.date).getTime() > timeDivider);
 	meetups.sort((a, b) => new Date(a.date).valueOf() - new Date(b.date).valueOf());
 
-	return meetups.slice(0, limit);
+	return limit ? meetups.slice(0, limit) : meetups;
 }
 
-export function getPastMeetups(limit = undefined) {
+// to not immediately show the next meetup as soon as it's over
+// add a buffer of X days
+export function getNextMeetup(bufferDays = 0) {
+	const timeDivider = new Date(new Date().getTime() - bufferDays * 24 * 60 * 60 * 1000);
+	return getNextMeetups(1, timeDivider)[0];
+}
+
+export function getPastMeetups(limit = undefined, timeDivider = undefined) {
+	if (!timeDivider) {
+		timeDivider = todayEndOfDay;
+	}
 	const meetups = allMeetups.filter((meetup) => new Date(meetup.date).getTime() <= timeDivider);
 	meetups.sort((a, b) => new Date(b.date).valueOf() - new Date(a.date).valueOf());
 
-	return meetups.slice(0, limit);
+	return limit ? meetups.slice(0, limit) : meetups;
 }
